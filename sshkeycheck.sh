0afe88f8fa3f6e27bf12ca0689101d4ee4606017
Carsten Lahme <carsten.lahme@dxc.com>
2018-12-06 17:05:53 +0100

    Update sshkeycheck.sh

--- sshkeycheck.sh
+++ sshkeycheck.sh
@@ -16,18 +16,18 @@ KeyIsKnown=$(grep "$(ssh-keyscan -t rsa $prodhost)" ~/.ssh/known-hosts|wc -l)
 #echo "KeyIsKnown= "  $KeyIsKnown  
 #echo "HostIsKnown= "  $HostIsKnown  
 if [  $KeyIsKnown -ne  $HostIsKnown ] || [  $(( $KeyIsKnown *  $HostIsKnown)) -gt 1 ]; then  
-	echo "99"  
+	echo -n "99"  
 	return "99"  
 else  
 	if [  $KeyIsKnown -eq 0 ]; then  
 		ssh-keyscan -t rsa $prodhost >> ~/.ssh/known-hosts  
-		echo "1"  
+		echo -n "1"  
 		return "1"  
 	else  
-		echo "0"  
+		echo -n "0"  
 		return "0"  
 	fi  
 fi
 ##script should never come so far.
-echo 57
-return 57
+echo -n "57"
+return "57"
