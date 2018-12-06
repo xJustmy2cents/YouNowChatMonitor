bce32204567fa2c1ea4c7dbfb3bed0f2e6c92ec2
Carsten Lahme <carsten.lahme@dxc.com>
2018-12-06 17:02:38 +0100

    Update sshkeycheck.sh

--- sshkeycheck.sh
+++ sshkeycheck.sh
@@ -13,15 +13,21 @@ if [ ! -e ~/.ssh/known-hosts ]; then
 HostIsKnown=$(grep "$prodhost" ~/.ssh/known-hosts|wc -l)  
 KeyIsKnown=$(grep "$(ssh-keyscan -t rsa $prodhost)" ~/.ssh/known-hosts|wc -l)  
 
-echo "KeyIsKnown= "  $KeyIsKnown  
-echo "HostIsKnown= "  $HostIsKnown  
+#echo "KeyIsKnown= "  $KeyIsKnown  
+#echo "HostIsKnown= "  $HostIsKnown  
 if [  $KeyIsKnown -ne  $HostIsKnown ] || [  $(( $KeyIsKnown *  $HostIsKnown)) -gt 1 ]; then  
+	echo "99"  
 	return "99"  
 else  
 	if [  $KeyIsKnown -eq 0 ]; then  
 		ssh-keyscan -t rsa $prodhost >> ~/.ssh/known-hosts  
+		echo "1"  
 		return "1"  
 	else  
+		echo "0"  
 		return "0"  
 	fi  
-fi  
+fi
+##script should never come so far.
+echo 57
+return 57
