3e2356d35d5449d297a5962248f0f2cc4c7df35c
xjustmy2cents <xjustmy2cents@somewhere.in.the.world.us>
2021-06-09 17:54:12 +0200

    Update loopchatDB.ps1

--- files/loopchatDB.ps1
+++ files/loopchatDB.ps1
@@ -52,99 +52,70 @@ if ($YNUSER -ne "") {
 
 	while ($MYERRORCODE -eq 0) {
 		$CURRENTCHAT20B64 = @()
-		$MYCONTENT=$($(wget https://api.younow.com/php/api/broadcast/info/user=$YNUSER).content|ConvertFrom-Json)
+		$MYCONTENT=$($(wget https://api.younow.com/php/api/broadcast/info/user=$($YNUSER.replace(".","%2e"))).content|ConvertFrom-Json)
 		$MYERRORCODE=$($MYCONTENT|select -expand errorcode)
 		if ( $MYERRORCODE -eq 0 ) {	
-			## RUNLOOPINIT START
-			if ( $RUNLOOPINIT -eq 0 ) {
-				$BROADCASTID = $($MYCONTENT.broadcastID)
-				$USERID=$($MYCONTENT.username)
-				$CURRENTFILE="$MYLOGPATH\$USERID-$BROADCASTID.log"
-				if ( $DEBUG ) {$DEBUGFILE="$MYLOGPATH\$USERID-$BROADCASTID.dbg"}
-				if ( -not (test-path $CURRENTFILE)) {
-					new-item $CURRENTFILE -ItemType file
-					} 
-				}
-			## RUNLOOPINIT END
-			
-			##Einlesen des aktuellen Chats
-			$CURRENTCHAT20=$($MYCONTENT|select -expand comments|select name, comment)
-			
-			## Kodieren des aktuellen Chat (BASE64)
-			foreach ($CURRENTCHAT in $CURRENTCHAT20) {
-				$CURRENTCHAT20B64 += "$(encodeb64($CURRENTCHAT.name + " " + $CURRENTCHAT.comment))"
-				}
-			
-			## holen des letzen gespeicherten Chat im Logfile (Dynamischer Block)
-			try {
-				$LASTCHAT20=$(Get-Content -last ($CURRENTCHAT20B64.length) $CURRENTFILE)
-				} catch {
-				$LASTCHAT20=$(Get-Content $CURRENTFILE)
-				}
-			#write-host 'loopchat: before BUILTCHAT: $LASTCHAT20.lenght=' $LASTCHAT20.length
-			## zerlegen des letzen Chat -> Zeitstempel abschneiden
-			$LASTCHAT20B64 = BUILTLASTCHAT -MYINPUT $LASTCHAT20
-			#write-host 'loopchat: after BUILTCHAT: $LASTCHAT20B64.lenght=' $LASTCHAT20B64.length
-			
-			## Ausgabe es letzten Chatblocks für Debugging
-			if ( $DEBUG ) {
-				$(get-date -format yyyMMddHHmmss)|out-file -append $DEBUGFILE
-				"`$CURRENTCHAT20B64`:"|out-file -append $DEBUGFILE
-				$CURRENTCHAT20B64|out-file -append $DEBUGFILE
-				"`$LASTCHAT20B64`:"|out-file -append $DEBUGFILE
-				$LASTCHAT20B64|out-file -append $DEBUGFILE
-				'================================'|out-file -append $DEBUGFILE
-				}
-			$CURRENTCHAT20B64_MAXINDEX=$CURRENTCHAT20B64.length -1
-			$LASTCHAT20B64_MAXINDEX=$LASTCHAT20B64.length -1
-			## ausgabe der Anzahl der Chat Zeilen im Vergleich
-			#write-host 'loopchat:$CURRENTCHAT20B64:' $CURRENTCHAT20B64
-			#write-host 'loopchat:$LASTCHAT20B64:' $LASTCHAT20B64
-			
-			
-			$CHATREFERENCE=$CURRENTCHAT20B64_MAXINDEX  ##lines to send to engine
-			for ( $i=0; $i -le $LASTCHAT20B64_MAXINDEX; $i++ ) {
-				if ( -not (compare-object $LASTCHAT20B64[$(0+$i)..$LASTCHAT20B64_MAXINDEX] $CURRENTCHAT20B64[0..$($CURRENTCHAT20B64_MAXINDEX - $i)]) ) {
-					$CHATREFERENCE=$i-1
-					break
-				}
-				if ( $DEBUG ) {
-					"`$LASTCHAT20B64[$(0+$i)..$LASTCHAT20B64_MAXINDEX]`:"|out-file -append $DEBUGFILE
-					$LASTCHAT20B64[$(0+$i)..$LASTCHAT20B64_MAXINDEX]|out-file -append $DEBUGFILE
-					"`$CURRENTCHAT20B64[0..$($CURRENTCHAT20B64_MAXINDEX - $i)]`:"|out-file -append $DEBUGFILE
-					$CURRENTCHAT20B64[0..$($CURRENTCHAT20B64_MAXINDEX - $i)]|out-file -append $DEBUGFILE
-				}
-			}
-			#### Logik Problem abfangen
-			#if ($CHATREFERENCE -gt $LASTCHAT20B64_MAXINDEX) {$CHATREFERENCE = -1}
+			#####################################
 			##
-			if ($CHATREFERENCE -gt -1) {
-				# write-host 'loopchat:$CURRENTCHAT20B64.length:' $CURRENTCHAT20B64_MAXINDEX
-				# write-host 'loopchat:$LASTCHAT20B64.length:' $LASTCHAT20B64_MAXINDEX
-				# write-host 'loopchat:$CHATREFERENCE: ' $CHATREFERENCE;
-				for ( $i=$CURRENTCHAT20B64_MAXINDEX - $CHATREFERENCE; $i -le $CURRENTCHAT20B64_MAXINDEX; $i++ ) {
-					# write-host '$CURRENTCHAT20B64['$i']: ' $CURRENTCHAT20B64[$i]
-					if ( $LASTCHAT20B64 -contains "$CURRENTCHAT20B64[$i]" -or $CURRENTCHAT20B64[0..$($i-1)] -contains "$CURRENTCHAT20B64[$i]" ) {
-						##do nothing
-						} else {
-						if ($READALL -or $RUNLOOPINIT -eq 1) {
-							if ( -not $NOTALK ) {newtalk -TALKTHIS $CURRENTCHAT20B64[$i] -SAYIT $SAYALL -WRITELOG $WRITELOG}
-							}
+			## Work stanza
+			##
+			#####################################
+			
+			if (!$THIS_BROADCASTID) {
+				##load broadcastid and check, if a log exists
+				$THIS_BROADCASTID = $MYCONTENT.broadcastid
+				$THIS_LOGFILE = "./outfiles/$THIS_BROADCASTID.log"
+				if ( test-path $THIS_LOGFILE ) {
+					try {
+						$THIS_SERVERTIME_OLD=$( $(get-content $THIS_LOGFILE|select -last 1).split(":"))[0]
+						} catch {
+						##file is empty, just skip
 						}
-					$(get-date -format yyyMMddHHmmss) + ";;" + $CURRENTCHAT20B64[$i] | out-file -append $CURRENTFILE
+				} else {
+					$null > $THIS_LOGFILE
+				}
+			}
+				
+			##Load current servertime and set old reference for first time
+			$THIS_SERVERTIME_NEW=$MYCONTENT.servertime
+			if (!$THIS_SERVERTIME_OLD) {$THIS_SERVERTIME_OLD=$THIS_SERVERTIME_NEW}
+
+
+			##Load the Comments in an array
+			$MYNEWCOMMENTS=$MYCONTENT|select -expand comments
+			
+			##Loop thought the comments and check by timestamp
+			foreach ($MYNEWCOMMENT in $MYNEWCOMMENTS) {
+				if ($MYNEWCOMMENT.timestamp -ge $THIS_SERVERTIME_OLD) {
+					## encrypt the comment to base64 and send it to the filter
+					$MYNEWCOMMENT.comment=$(removeem($MYNEWCOMMENT.comment))
+					$THIS_COMMENT_USERID=$MYNEWCOMMENT.userid
+					
+					##because of issues with the out-file, we need to resolve all objects to a simple variable
+					$THIS_COMMENT_USERID_INFO=$($(wget https://cdn.younow.com/php/api/channel/getInfo/channelId=$THIS_COMMENT_USERID).content|convertfrom-json)
+					$COL01=$THIS_SERVERTIME_OLD
+					$COL02=$MYNEWCOMMENT.timestamp
+					$COL03= -join($MYNEWCOMMENT.name,$(if ($THIS_COMMENT_USERID_INFO.datecreated -like "2020-08-10*") {$("(BOT)")}))
+					$COL04=$MYNEWCOMMENT.comment
+					$COL05=$THIS_COMMENT_USERID_INFO.userid
+					$COL06=$THIS_COMMENT_USERID_INFO.datecreated
+					$COL07=$THIS_COMMENT_USERID_INFO.propslevel
+					$COL08=""
+					$COL09=""
+					
+					
+					write-host $COL01":"$COL02":"$COL03":"$COL04":"$COL05":"$COL06":"$COL07
+					write $COL01":"$COL02":"$COL03":"$COL04":"$COL05":"$COL06":"$COL07|out-file $THIS_LOGFILE -append
+					
 					}
+				}
+			
+			##roll servertime reference
+			$THIS_SERVERTIME_OLD = $THIS_SERVERTIME_NEW
+
 			}
 			
 			
-			
-			
-			
-			
-
-
-			$RUNLOOPINIT = 1
-			start-sleep 0.6
-			}
 			switch ($MYERRORCODE) {
 				"206"  {
 					if ( $WAIT4STREAM ) {
@@ -155,6 +126,7 @@ if ($YNUSER -ne "") {
 						}
 					}
 				}
+				sleep 0.6
 		}
 	
 	switch ($MYERRORCODE) {
